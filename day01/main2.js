029bee92aff827b7c800258b6df599338fbd8372
serpean <sergio.perez@seidor.com>
2022-12-02 08:09:54 +0100

    day01 completed

--- day01/main2.js
+++ day01/main2.js
@@ -2,26 +2,19 @@ const fs = require('fs');
 const path = require('path');
 const input = fs.readFileSync(path.join(__dirname, 'input1.txt'), 'utf8');
 const numbers = input.split('\n');
-let top = 0;
-console.log(numbers)
 
-
+const stack = {}
 
 for (let i = 0; i < numbers.length; i++) {
     let sum = 0;
     let j = i;
-    console.log("i", i, numbers[i], numbers[i] !== '');
 
     while (numbers[j] && numbers[j] !== '') {
-        console.log("j", j, numbers[j], numbers[j] !== '');
         sum += parseInt(numbers[j]);
         j++;
     }
-
-
-    if (sum > top) {
-        top = sum;
-    }
+    stack[sum] = 1;
 }
 
-console.log(top);
+console.log(Object.keys(stack).slice(-3));
+console.log(Object.keys(stack).slice(-3).reduce((acc, curr) => acc + parseInt(curr), 0));
